a0ebfbe6d3f29bf00f7b931e7c5f4fa97f0548be
yangli <yangli1@xiaomi.com>
2017-08-16 11:18:14 +0800

    配置Native模块

---
+++ videoeditor/src/main/java/com/thornbirds/videoeditor/VideoRecorder.java
@@ -0,0 +1,15 @@
+package com.thornbirds.videoeditor;
+
+/**
+ * Created by yangli on 2017/8/15.
+ *
+ * @module 视频录制器
+ */
+public class VideoRecorder {
+
+    static {
+        System.loadLibrary("video_recorder-lib");
+    }
+
+    public native String stringFromJNI();
+}
